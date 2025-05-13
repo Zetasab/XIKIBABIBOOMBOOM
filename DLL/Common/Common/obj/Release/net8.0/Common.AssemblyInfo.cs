d0bef16913d499d26ae14ff2dc70e424e8eb7b3c
cesarsobrino <cesassab@gmail.com>
2025-05-13 18:33:10 +0200

    Update docs

--- DLL/Common/Common/obj/Release/net8.0/Common.AssemblyInfo.cs
+++ DLL/Common/Common/obj/Release/net8.0/Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c193c596c4a53ff268b2258165963770600e0db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbd6b7d60bef89d2c5147b9dbc7fdc8832b44512")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
