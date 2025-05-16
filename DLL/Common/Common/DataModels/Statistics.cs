21bd451f9e150d2252c7d66c046a8fe9ab39ccc4
cesarsobrino <cesassab@gmail.com>
2025-05-16 16:50:34 +0200

    Mudblazors

---
+++ DLL/Common/Common/DataModels/Statistics.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.DataModels
+{
+    public class Statistics
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public Statistics() { }
+
+        public Statistics(string name, string description, DateTime startDate)
+        {
+            Id = Guid.NewGuid().ToString();
+            Name = name;
+            Description = description;
+            StartDate = startDate;
+        }
+    }
+}
--- DLL/Common/Common/obj/Debug/net8.0/Common.AssemblyInfo.cs
+++ DLL/Common/Common/obj/Debug/net8.0/Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c193c596c4a53ff268b2258165963770600e0db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c65f00966d4d4b5d3620c4e092a188338156981c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
